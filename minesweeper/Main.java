a09194b830fb7f86397db5c81d1ecb3f452c253a
zaydpathan <zaydnpathan@gmail.com>
2026-09-17 21:08:20 -0400

    Update Main.java

--- minesweeper/Main.java
+++ minesweeper/Main.java
@@ -1,6 +1,56 @@
 package minesweeper;
+import java.util.Scanner;
 public class Main
+/**
+ * 
+ */
 {
+    /**
+     * Runs the main game loop
+     * @param args this lets us know what kind of game are we doing
+     * whether its going to be a custom game or regular game
+     * 
+     */
+    public void main(String[] args)
+    {
+        
+    }
+    /**
+     * creates a new game
+     * @param scanner this scans the user input to see what the coordinates are
+     * @return returns a board in which the new game will be played on
+     */
+    public board newGame(Scanner scannner)
+    {
+        System.out.print("Enter your what difficulty you want easy medium or hard: ");
+        scanner.next();
+        
+       //create if statemtents going through if easy, medium, or hard 
+       Board boardNew = new Board();
+       main.create
+        
+    }
+    /**
+     * creates a new custom game
+     * @param scanner this scans the user input to see what the coordinates are
+     * @return returns a custom board in which the new game will be played on
+     */
+    public void customGame(Scanner scanner, Board board)
+    {
+        
+    }
+    public String getUserInput(Scanner scanner)
+    {
+        
+    }
+    public boolean scannerWin(Scanner scanner)
+    {
+        
+    }
+    public boolean scannerLose(Scanner scanner)
+    {
+        
+    }
     //~ Fields ................................................................
 
     //~ Constructors ..........................................................
