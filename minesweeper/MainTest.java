99364fcc57aa4a69debb1bb42ff665b73d202fb4
zaydpathan <zaydnpathan@gmail.com>
2026-09-22 18:29:10 -0400

    Update MainTest.java

--- minesweeper/MainTest.java
+++ minesweeper/MainTest.java
@@ -5,11 +5,46 @@ public class MainTest
 {
 
     @Test
-    public void test()
+    public void testMain()
     {
         fail("Not yet implemented");
     }
     //~ Fields ................................................................
+
+
+    @Test
+    public void testNewGame()
+    {
+        fail("Not yet implemented");
+    }
+
+
+    @Test
+    public void testCustomGame()
+    {
+        fail("Not yet implemented");
+    }
+
+
+    @Test
+    public void testGetUserInput()
+    {
+        fail("Not yet implemented");
+    }
+
+
+    @Test
+    public void testScannerWin()
+    {
+        fail("Not yet implemented");
+    }
+
+
+    @Test
+    public void testScannerLose()
+    {
+        fail("Not yet implemented");
+    }
 
     //~ Constructors ..........................................................
 
