d3fc773a53b1fe42918e24366fc8dd63cbded853
zaydpathan <zaydnpathan@gmail.com>
2026-09-24 09:37:08 -0400

    Update MainTest.java

--- minesweeper/MainTest.java
+++ minesweeper/MainTest.java
@@ -2,15 +2,28 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MainTest
+
 {
 
+    private Main game;
+    
+    @Before
+    public void setUp() {
+        game = new Main();
+    }
     @Test
     public void testMain()
     {
         fail("Not yet implemented");
     }
     //~ Fields ................................................................
-
+    @Test
+    public void testNewGameEasy() {
+        Board b = game.newGame(in("easy"));
+        assertNotNull(b);
+        assertNotNull(b.getTile(8, 8));
+        assertFalse(b.inBounds(9, 9));
+    }
 
     @Test
     public void testNewGame()
